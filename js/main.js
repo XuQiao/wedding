cee15ea22876bad839911a001bb5a9c43bc75978
XuQiao <xu.qiao93@gmail.com>
2025-09-02 14:14:02 +0800

    m

--- js/main.js
+++ js/main.js
@@ -102,23 +102,3 @@ document.addEventListener("DOMContentLoaded", function() {
         setInterval(updateCountdown, 1000);
     }
 });
-
-function updateCountdown() {
-    const weddingDate = new Date("2023-10-01T00:00:00");
-    const now = new Date();
-    const diff = weddingDate - now;
-    
-    const days = Math.floor(diff / (1000 * 60 * 60 * 24));
-    const hours = Math.floor((diff % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
-    const minutes = Math.floor((diff % (1000 * 60 * 60)) / (1000 * 60));
-    const seconds = Math.floor((diff % (1000 * 60)) / 1000);
-    
-    /*
-    document.getElementById("countdown-timer").innerHTML = `
-        <div><span>天</span>${days}</div>
-        <div><span>时</span>${hours}</div>
-        <div><span>分</span>${minutes}</div>
-        <div><span>秒</span>${seconds}</div>
-    `;
-    */
-}
--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,9 @@
 import { defineConfig } from 'vite';
 
 export default defineConfig({
-  base: '/wedding/', // 替换为你的仓库名
+  base: '/wedding', // 替换为你的仓库名
   build: {
     outDir: './dist',
-    sourcemap: false,
-    module: 'system'
-  }
+    sourcemap: false
+    }
 });
