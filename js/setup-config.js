2f689119448202ffb15f6c4b72f3a9d07e1780b4
XuQiao <xu.qiao93@gmail.com>
2025-09-02 10:01:18 +0800

    m

--- js/setup-config.js
+++ js/setup-config.js
@@ -4,8 +4,8 @@ const path = require('path');
 
 const config = {
   GH_TOKEN: process.env.GH_TOKEN,
-  APP_ID: process.env.APP_ID,
-  APP_TOKEN: process.env.APP_TOKEN
+  //APP_ID: process.env.APP_ID,
+  //APP_TOKEN: process.env.APP_TOKEN
 };
 
 // 读取模板文件
@@ -14,14 +14,14 @@ let template = fs.readFileSync(templatePath, 'utf8');
 
 // 替换占位符
 template = template.replace('/* GH_TOKEN */', `"${config.GH_TOKEN}"`)
-template = template.replace('/* APP_ID */', `"${config.APP_ID}"`)
-template = template.replace('/* APP_TOKEN */', `"${config.APP_TOKEN}"`)
-template = "config.js"
+//template = template.replace('/* APP_ID */', `"${config.APP_ID}"`)
+//template = template.replace('/* APP_TOKEN */', `"${config.APP_TOKEN}"`)
+
 // 写入实际配置文件
 const outputPath = path.join(__dirname, '../js/config.js');
 fs.writeFileSync(outputPath, template);
 
 console.log('Configuration file generated successfully', template, outputPath);
-const noJekyllFilePath = path.join(__dirname, '..', '.nojekyll');
-fs.writeFileSync(noJekyllFilePath, '');
-console.log('.nojekyll 文件已创建');
+//const noJekyllFilePath = path.join(__dirname, '..', '.nojekyll');
+//fs.writeFileSync(noJekyllFilePath, '');
+//console.log('.nojekyll 文件已创建');
