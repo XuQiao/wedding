4e14d2d90bf0f7e233d4d16229a5f6f99f55ecdf
XuQiao <xu.qiao93@gmail.com>
2025-09-02 14:18:13 +0800

    m

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,18 @@
 import { defineConfig } from 'vite';
+import { resolve } from 'path'
 
 export default defineConfig({
   base: '/wedding', // 替换为你的仓库名
   build: {
     outDir: './dist',
-    sourcemap: false
+    sourcemap: false,
+    rollupOptions: {
+      input: {
+        main: resolve(__dirname, 'index.html'),
+        map: resolve(__dirname, 'map.html'),
+        schedule: resolve(__dirname, 'schedule.html'),
+        guestbook: resolve(__dirname, 'guestbook.html')
+      }
     }
+  }
 });
