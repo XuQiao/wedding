9858d32919776c866a2dff2e04ecca41811ce5fc
Xu Qiao <xu.qiao93@gmail.com>
2025-09-21 20:25:23 +0800

    m

--- js/map.js
+++ js/map.js
@@ -3,12 +3,14 @@
 function initMap() {
     // 创建地图实例
     const map = new AMap.Map('map-container', {
-        zoom: 15,
+        zoom: 14,
         center: [118.740218,30.97028],  // 敬亭湖宾馆坐标
+        //center: [118.7355,30.97222],
         viewMode: '2D',
         pitch: 45
     });
 
+    /*
     // 添加3D建筑
     map.on('complete', function() {
         AMap.plugin(['AMap.Buildings'], function() {
@@ -18,17 +20,19 @@ function initMap() {
             }));
         });
     });
-
+    */
     // 添加婚礼场地标记
     const marker = {
         "banquet": new AMap.Marker({
         position: [118.737997,30.972957],
+        //position: [118.73473,30.97488],
         map: map,
         icon: 'https://webapi.amap.com/theme/v1.3/markers/n/mark_r.png',
         title: '婚礼主场地 -- 研学基地内部草坪'
         }),
         "venue": new AMap.Marker({
         position: [118.740218,30.97028],
+        //position: [118.7355,30.97222],
         map: map,
         icon: 'https://webapi.amap.com/theme/v1.3/markers/n/mark_b.png',
         title: '午宴场地 -- 敬亭湖宾馆通和西厅'
@@ -43,7 +47,8 @@ function initMap() {
                     <p>2025年10月19日 10:28</p>
                     <img src="./images/wedding-icon.png" alt="" />
                   </div>`,
-        offset: new AMap.Pixel(0, -30)
+        offset: new AMap.Pixel(0, -30),
+        automove: true
     }),
         "venue": new AMap.InfoWindow({
         content: `<div class="map-infowindow">
@@ -51,14 +56,21 @@ function initMap() {
                     <p>2025年10月19日 12:08</p>
                     <img src="./images/wedding-icon.png" alt="" />
                   </div>`,
-        offset: new AMap.Pixel(0, -30)
+        offset: new AMap.Pixel(0, -30),
+        automove: true
         })
     }
 
     // 点击标记显示信息窗口
     Object.entries(marker).forEach(([key, value]) => {
         value.on('click', function() {
-            infoWindow[key].open(map, item.getPosition());
+            console.log(key, value.getPosition())
+            map.setCenter(value.getPosition());
+            value.setPosition(value.getPosition());
+            setTimeout(function(){
+                infoWindow[key].open(map, value.getPosition())
+            }, 400);
+            map.setZoomAndCenter(map.getZoom()>13?map.getZoom():14,value.getPosition());//设置地图放大级别及居中
         })
     })
     
@@ -70,11 +82,16 @@ function initMap() {
     // POI点交互
     document.querySelectorAll('.poi-item').forEach(item => {
         item.addEventListener('click', function() {
-            const lng = parseFloat(this.dataset.lng);
-            const lat = parseFloat(this.dataset.lat);
-            map.setCenter([lng, lat]);
-            marker[this.dataset.type].setPosition([lng, lat]);
-            infoWindow[this.dataset.type].open(map, [lng, lat]);
+            var ds = this.dataset
+            const lng = parseFloat(ds.lng);
+            const lat = parseFloat(ds.lat);
+            console.log(lng, lat)
+            //map.setCenter([lng, lat]);
+            map.setZoomAndCenter(map.getZoom()>13?map.getZoom():14,[lng, lat]);//设置地图放大级别及居中
+            //marker[ds.type].setPosition([lng, lat]);
+            setTimeout(function(){
+                infoWindow[ds.type].open(map, [lng, lat])
+            }, 400);
         });
     });
 
